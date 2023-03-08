568669c8084b49748ffea6e80c0eb72334d1f48c
Dawid Wrobel <118534055+Dawidos12352@users.noreply.github.com>
2023-03-08 20:40:54 +0100

    gallery ready to download

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery  = document.querySelector("div.gallery");
+const galleryArray = [];
+
+
+galleryItems.forEach(event => {
+const galleryItem = document.createElement("div");
+galleryItem.classList = "gallery__item";
+
+const galleryLink = document.createElement("a");
+galleryLink.classList = "gallery__link";
+galleryLink.href = event.original;
+
+const galleryImage = document.createElement("img");
+galleryImage.classList = "gallery__image"
+galleryImage.src = event.preview;
+galleryImage.alt = event.description;
+galleryImage.setAttribute("data-source", event.orginal);
+
+galleryItem.append(galleryLink);
+galleryLink.append(galleryImage);
+galleryArray.push(galleryItem);
+
+});
+
+gallery.append(...galleryArray);
+
 console.log(galleryItems);
