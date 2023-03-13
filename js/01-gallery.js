ebd6423f178147cea474ab8522c7d785e76f4376
Dawid Wrobel <118534055+Dawidos12352@users.noreply.github.com>
2023-03-13 20:12:50 +0100

    finish 2 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,48 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery  = document.querySelector("div.gallery");
+const gallery = document.querySelector(".gallery");
 const galleryArray = [];
 
+galleryItems.forEach( event => {
 
-galleryItems.forEach(event => {
 const galleryItem = document.createElement("div");
-galleryItem.classList = "gallery__item";
+galleryItem.className = "gallery__item";
 
 const galleryLink = document.createElement("a");
-galleryLink.classList = "gallery__link";
+galleryLink.className = "gallery__link";
 galleryLink.href = event.original;
 
 const galleryImage = document.createElement("img");
-galleryImage.classList = "gallery__image"
+galleryImage.className = "gallery__image";
 galleryImage.src = event.preview;
-galleryImage.alt = event.description;
 galleryImage.setAttribute("data-source", event.original);
+galleryImage.alt = event.description;
 
 galleryItem.append(galleryLink);
 galleryLink.append(galleryImage);
 galleryArray.push(galleryItem);
-
 });
 
 gallery.append(...galleryArray);
 
-gallery.addEventListener("click", e => {
-    e.preventDefault();
-    if (e.target.nodeName !== "IMG") return;
-    const imageDataSource = e.target.getAttribute("data-source");
+gallery.addEventListener( "click", event => {
+    event.preventDefault();
 
-    const instance = basicLightbox.create(
-        `<img src="${imageDataSource}" width="800" height="600">`,
-        {
-            onShow: (instance) => {
-                document.addEventListener("keydown", (e) => {
-                    if (e.key === "Escape") instance.close();
-                });
-            }
-        });
-    
-    instance.show();
+if (event.target.nodeName !== "IMG") {
+    return;
+}
+const dataSourceImage = event.target.getAttribute("data-source");
 
+const instance = basicLightbox.create(
+    `<img src="${dataSourceImage}" width="800" height="600">`, {
+    onShow: (instance) => {
+document.addEventListener( "keydown", (event) =>{
+    if (event.key === "Escape") instance.close();
+});
+    }}
+    );
+
+instance.show();
 });
 
